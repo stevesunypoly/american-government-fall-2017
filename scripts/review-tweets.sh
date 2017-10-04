df85e42faf2b939b8a2e314bec43b4d3451fd9c9
Steve Schneider <steve@sunyit.edu>
2017-10-04 15:27:39 -0400

    lots of updates
    lots of updates

--- scripts/review-tweets.sh
+++ scripts/review-tweets.sh
@@ -1,4 +1,4 @@
-for file in ../pos110-fall2017/tiddlers/tweets/Box\ Sync/tweets/*.txt; do mv "$file" "${file%.txt}.tid"; done
-tiddlywiki ../pos110-fall2017 --verbose --load ../pos110-fall2017/tiddlers/tweets/Box\ Sync/tweets/*.tid
+for file in ../../american-politics-fall-2017/pos110-fall2017/tiddlers/tweets/Box\ Sync/tweets/*.txt; do mv "$file" "${file%.txt}.tid"; done
+tiddlywiki  ../pos110-fall2017 --verbose --load ../../american-politics-fall-2017/pos110-fall2017/tiddlers/tweets/Box\ Sync/tweets/*.tid
 tiddlywiki ../pos110-fall2017 --verbose --server
 
